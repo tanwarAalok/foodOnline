60ef3fb5fe70ff0355fa7ef35823c7b4ccf064da
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-10-19 09:27:36 +0530

    registerUser setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
+
+def registerUser(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.set_password(password)
+            user.save()
+
+            messages.success(request, "Your account has been registered successfully !")
+            return redirect('registerUser')
+
+    else:
+        form = UserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register-user.html', context)
--- foodz_main/settings.py
+++ foodz_main/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 from decouple import config
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,3 +120,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
